a3322ccb5751ced01ec70785c47aad098fad303c
Zack Dowd <zdowd2796@gmail.com>
2023-07-19 19:13:32 -0600

    enemy spawn cleaning itself up

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -7,14 +8,21 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private List<GameObject> enemies = new List<GameObject>();
     [SerializeField] private float timeToSpawn;
     [SerializeField] private Transform minSpawn, maxSpawn;
+    [SerializeField] private int checkPerFrame;
+    [SerializeField] private GameObject enemyPool;
+    private int enemyToCheck;
     private float spawnCounter;
     private Transform target;
+    private float despawnDistance;
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
 
     void Start()
     {
         spawnCounter = timeToSpawn;
 
         target = PlayerHealthController.Instance.transform;
+
+        despawnDistance = Vector3.Distance(transform.position, maxSpawn.position) + 4f;
     }
 
     void Update()
@@ -24,9 +32,31 @@ public class EnemySpawner : MonoBehaviour
         if(spawnCounter < 0) {
             spawnCounter = timeToSpawn;
             //Instantiate(enemies[randomEnemy], transform.position, Quaternion.identity);
-            Instantiate(enemies[randomEnemy], SelectSpawnPoint(), Quaternion.identity);
+            GameObject newEnemy = Instantiate(enemies[randomEnemy], SelectSpawnPoint(), Quaternion.identity, enemyPool.transform);
+            spawnedEnemies.Add(newEnemy);
         }
         transform.position = target.position;
+
+        int checkTarget = enemyToCheck + checkPerFrame;
+        while(enemyToCheck < checkTarget) {
+            if(enemyToCheck < spawnedEnemies.Count) {
+                if (spawnedEnemies[enemyToCheck] != null) {
+                    if(Vector3.Distance(transform.position, spawnedEnemies[enemyToCheck].transform.position) > despawnDistance) {
+                        Destroy(spawnedEnemies[enemyToCheck]);
+                        spawnedEnemies.RemoveAt(enemyToCheck);
+                        checkTarget--;
+                    } else {
+                        enemyToCheck++;
+                    }
+                } else {
+                    spawnedEnemies.RemoveAt(enemyToCheck);
+                    checkTarget--;
+                }
+            } else {
+                enemyToCheck = 0;
+                checkTarget = 0;
+            }
+        }
     }
 
     private Vector3 SelectSpawnPoint() {
