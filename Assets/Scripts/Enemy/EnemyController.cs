3e68fc4c12f286b3a02ea8a7f25ba9c5a6a4fd2e
Zack Dowd <zdowd2796@gmail.com>
2023-07-19 18:14:47 -0600

    Health for player implemented

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-
+    [SerializeField] private float damageDone;
+    [SerializeField] private float hitWaitTime = 1f;
+    private float hitCounter;
     private Rigidbody2D rb;
     private Transform target;
-
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +20,17 @@ public class EnemyController : MonoBehaviour
 
     private void Update() {
         rb.velocity = (target.position - transform.position).normalized;
+
+        if(hitCounter > 0f) {
+            hitCounter -= Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player") && hitCounter <= 0) {
+            PlayerHealthController.Instance.TakeDamage(damageDone);
+
+            hitCounter = hitWaitTime;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerHealthController.cs
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static PlayerHealthController Instance { get; private set; }
+
+    [SerializeField] private float currentHealth, maxHealth;
+    [SerializeField] private Image healthBarImage;
+
+
+    private void Awake() {
+        if(Instance != null) {
+            Debug.LogError("There is already a HealthController in the scene");
+        }
+        Instance = this;
+    }
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthBarImage.fillAmount = (float)currentHealth / maxHealth;
+        if(Input.GetKeyDown(KeyCode.P)) {
+            TakeDamage(5);
+        }
+    }
+
+    public void TakeDamage(float damage) {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0 ) {
+            //Die
+            gameObject.SetActive(false);
+        }
     }
 }
