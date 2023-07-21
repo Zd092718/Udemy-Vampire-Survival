a5e55d670a0a4d0539b1aa645e40bc41223d2ce7
Zack Dowd <zdowd2796@gmail.com>
2023-07-21 12:05:49 -0600

    levelling up to lvl 99 working

--- Assets/Scripts/Player/ExperienceLevelController.cs
+++ Assets/Scripts/Player/ExperienceLevelController.cs
@@ -7,6 +7,8 @@ public class ExperienceLevelController : MonoBehaviour
     public static ExperienceLevelController Instance { get; private set; }
 
     [SerializeField] private int currentExperience;
+    [SerializeField] private List<int> expLevels;
+    [SerializeField] private int currentLevel = 1, levelCount = 100;
 
     private void Awake() {
         if(Instance != null) {
@@ -15,7 +17,27 @@ public class ExperienceLevelController : MonoBehaviour
         Instance = this;
     }
 
+    private void Start() {
+        while(expLevels.Count < levelCount) {
+            expLevels.Add(Mathf.CeilToInt(expLevels[expLevels.Count - 1] * 1.1f));
+        }
+    }
+
     public void GetExp(int amountToGet) {
         currentExperience += amountToGet;
+
+        if(currentExperience >= expLevels[currentLevel]) {
+            LevelUp();
+        }
+    }
+
+    public void LevelUp() {
+        currentExperience -= expLevels[currentLevel];
+
+        currentLevel++;
+
+        if(currentLevel >= expLevels.Count) {
+            currentLevel = expLevels.Count - 1;
+        }
     }
 }
